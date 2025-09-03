f79d1632b16c151830718c9544def2866f304d55
Alain Calderon <ajcalderon171999@gmail.com>
2025-09-03 22:11:47 +0800

    removed empty folders

--- src/app/_components/nav.tsx
+++ src/app/_components/nav.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 export default function Nav() {
   return (
     <div className="grid grid-cols-2 mx-14  bg-black w-full">
-
       <nav className="cols-span-8">
         <Link href="/log-in">Log In</Link>
         <Link href="/sign-out">Sign out</Link>

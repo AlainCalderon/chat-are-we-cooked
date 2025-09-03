6cefbb7da05830247addd33d4365f84a6b556af0
Alain Calderon <ajcalderon171999@gmail.com>
2025-09-03 22:03:45 +0800

    attempt to fix is not module error

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -4,7 +4,7 @@ export default function Journal(){
     return(
         // conditional component. If user journals are empty show "Currently no journals" else list journals in a card format ( see inspo page )
         <>
-            <div></div>
+            <div><h1>Journals</h1></div>
 
             <div></div>
         </>

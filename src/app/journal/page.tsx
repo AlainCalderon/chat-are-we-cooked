c8a05d986db6934bdf5ab6bdd040eb37c6df6ac5
Alain Calderon <ajcalderon171999@gmail.com>
2025-09-19 12:27:16 +0800

    changed supabase key param

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -1,13 +1,23 @@
 import { createClient } from "@supaORM/server";
 import { redirect } from "next/navigation";
+import { fetchJournals } from "@lib/serverActions";
 export default async function Journal() {
   const supabase = await createClient();
+  const userJournals = await fetchJournals();
   const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
     redirect("/login");
   }
 
-  
+  if (!userJournals) {
+    return (
+      <>
+        <div>
+          <h2>You currently have no journals</h2>
+        </div>
+      </>
+    );
+  }
 
   return (
     // conditional component. If user journals are empty show "Currently no journals" else list journals in a card format ( see inspo page )
